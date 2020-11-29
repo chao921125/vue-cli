06d10a367cd5ae5b906a1776998361d3a926b146
huangchao <chao921125@sina.com>
2020-11-29 23:20:41 +0800

    基线

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,30 @@
 <template>
-  <a-layout id="components-layout-demo-custom-trigger">
-    <!-- eslint-ignore -->
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+  <a-layout class="components-layout">
+    <a-layout-sider
+      v-model:collapsed="collapsed"
+      :trigger="null"
+      collapsible
+      breakpoint="lg"
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+      :style="{ overflow: 'auto', height: 'calc(100vh-64px)', minHeight: '600px' }"
+    >
       <div class="logo" />
       <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
-        <a-menu-item key="1">
+        <a-menu-item v-for="item in 20" :key="item">
           <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
+          <span>nav {{ item }}</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
+      <a-layout-header style="background: #fff; padding: 0" :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
         <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
         <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
       </a-layout-header>
-      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '600px', marginTop: '84px' }"
+      >
         {{ testVal }}
         <router-view />
       </a-layout-content>
@@ -63,26 +64,34 @@ export default {
     toggleCollapse() {
       this.isCollapse = !this.isCollapse;
     },
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
-#components-layout-demo-custom-trigger .trigger {
-  font-size: 18px;
-  line-height: 64px;
-  padding: 0 24px;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-
-#components-layout-demo-custom-trigger .trigger:hover {
-  color: #1890ff;
-}
-
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
+.components-layout {
+  height: 100vh;
+  min-height: 700px;
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+  .trigger:hover {
+    color: #1890ff;
+  }
+  .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
 }
 </style>
