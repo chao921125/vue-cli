2e9dd638c9c2ddfe1509e05f2308f8859e1ab392
huangchao <chao.huang@advokategroup.com>
2021-02-01 15:24:10 +0800

    no message

---
+++ src/views/demo/timer/timer.vue
@@ -0,0 +1,33 @@
+
+    countDowm() {
+      let self = this;
+      clearInterval(this.promiseTimer);
+      this.promiseTimer = setInterval(function() {
+        if (self.hour === 0) {
+          if (self.minute !== 0 && self.second === 0) {
+            self.second = 59;
+            self.minute -= 1;
+          } else if (self.minute === 0 && self.second === 0) {
+            self.second = 0;
+            self.$emit('countDowmEnd', true);
+            clearInterval(self.promiseTimer);
+          } else {
+            self.second -= 1;
+          }
+        } else {
+          if (self.minute !== 0 && self.second === 0) {
+            self.second = 59;
+            self.minute -= 1;
+          } else if (self.minute === 0 && self.second === 0) {
+            self.hour -= 1;
+            self.minute = 59;
+            self.second = 59;
+          } else {
+            self.second -= 1;
+          }
+        }
+      }, 1000);
+    },
+    formatNum(num) {
+      return num < 10 ? '0' + num : '' + num;
+    }
