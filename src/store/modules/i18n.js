ea7ca9f3276072a487983e0749583c7b2caf3bcf
黄超 <chao.huang@advokategroup.com>
2021-04-19 11:15:59 +0800

    update

--- src/store/modules/i18n.js
+++ src/store/modules/i18n.js
@@ -4,7 +4,7 @@ export default {
   namespaced: true,
   // 对数据的全局存储
   state: {
-    i18n: Storage.getLocalItem("i18n") || "zh-cn",
+    i18n: Storage.getLocalItem("i18n") || "zh-CN",
   },
   // 获取声明的属性值，实时监听动态变化，对数据进行计算
   getters: {
