c3c1316491cefe3d640378eb10809731afd2ee05
黄超 <chao.huang@advokategroup.com>
2021-05-06 14:39:58 +0800

    update 其他路由

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <router-link to="/500" class="cc-mr-20">500</router-link>
     <router-link to="/error">error</router-link>
   </div>
-  <div>模仿vue2的filter：$filters.method <p v-for="item in 10" :key="item">{{ item + "=" + $filters.testFilter(item) }}</p></div>
+  <div>模仿vue2的filter，在3中写成了全局函数，调用方式：$filters.method <p v-for="item in 10" :key="item">{{ item + "=" + $filters.testFilter(item) }}</p></div>
 </template>
 
 <script>
