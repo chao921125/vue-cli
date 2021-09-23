40622d70bfd484e8a255628b709848e09eec284e
黄超 <chao.huang@advokategroup.com>
2021-09-23 18:37:46 +0800

    新的标签

--- src/views/demo/icon/Icon.vue
+++ src/views/demo/icon/Icon.vue
@@ -1,5 +1,6 @@
 <template>
   <el-row>
+    <el-col :span="24"><h1>以下是对应vue2的写法</h1></el-col>
     <el-col :span="24"><h1>EL icon</h1></el-col>
     <el-col :span="24">使用 Icon 图标参考官方给出的demo</el-col>
     <el-col :span="24" v-text="iconText"></el-col>
@@ -7,6 +8,9 @@
     <el-col :span="24">使用 Iconfont 图标参考官方给出的demo</el-col>
     <el-col :span="24" v-text="iconFontText"></el-col>
     <el-col :span="24"><h6><i><em>请注意使用上的区别，以及各个UI对i的一些样式处理</em></i></h6></el-col>
+    <el-col :span="24"><h1>以下是对应vue3的写法，同时依然支持vue2的写法，但是不知道什么时候就不支持了</h1></el-col>
+    <el-col :span="24"><h6><i><em>el-plus上面的icon使用方法已经变了，需要单独安装</em></i></h6></el-col>
+    <el-col :span="24" v-text="iconFontText3"></el-col>
   </el-row>
 </template>
 
@@ -16,7 +20,8 @@ export default {
   data() {
     return {
       iconText: "<i class='el-*'></i>",
-      iconFontText: "<i class='icon-*'></i>"
+      iconFontText: "<i class='icon-*'></i>",
+      iconFontText3: "<el-icon><alarm-clock /></el-icon>"
     }
   }
 };
