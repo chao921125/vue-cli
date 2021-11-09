ad5d129d73023e6b0677b0382ed3e0855989f9f5
黄超 <chao.huang@advokategroup.com>
2021-11-09 11:28:09 +0800

    add other echarts

--- src/views/demo/echarts/Echarts.vue
+++ src/views/demo/echarts/Echarts.vue
@@ -4,6 +4,23 @@
       <div id="chartDemo" ref="chartDemo" class="chart-box"></div>
     </el-col>
   </el-row>
+  <el-row>
+    <el-col :span="24">
+      <p>1、npm i element-resize-detector</p>
+      <p>2、import elementResizeDetectorMaker from 'element-resize-detector';</p>
+      <p>3、const erdm = elementResizeDetectorMaker();</p>
+      <p>4、erdm.listenTo(document.getElementById(''), (element) => {
+        const width = element.offsetWidth;
+        const height = element.offsetHeight;
+        that.$nextTick(() => {
+          // 使echarts尺寸重置
+          echarts.resize({
+            width, height
+          });
+        });
+      });</p>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
