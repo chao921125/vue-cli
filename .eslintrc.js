5b6d1f4952d19e833404f9faa5d8c7b14dfce7c6
chao <chao921125@sina.com>
2023-05-23 22:12:06 +0800

    更新配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    browser: true,
-    es6: true
-  },
-  extends: ["plugin:vue/essential", "plugin:prettier/recommended", "eslint:recommended", "@vue/prettier"],
-  plugins: ['vue', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'generator-star-spacing': 'off',
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint",
-    sourceType: 'module'
-  },
-  overrides: [
-    {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
-      env: {
-        mocha: true
-      }
-    }
-  ]
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es2022: true,
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended", "@vue/prettier"],
+	plugins: ["vue", "prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		"generator-star-spacing": "off",
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"vue/multi-word-component-names": "off",
+		"vue/no-reserved-component-names": "off",
+	},
+	parserOptions: {
+		parser: "@babel/eslint-parser",
+		sourceType: "module",
+	},
+	overrides: [
+		{
+			files: ["**/__tests__/*.{j,t}s?(x)"],
+			env: {
+				mocha: true,
+			},
+		},
+	],
 };
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
+# 本项目停止更新，请转移到[VUE3+VITE 版本](https://github.com/chao921125/vue-vite)
+
 # 使用
+
 ```
+corepack enable
+
 yarn install
 
-npm run dev
+npm run serve
 ```
